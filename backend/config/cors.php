08dccbb9b62e6e82a58e6d57ae7e06aa5b80abb5
jigar-tecmantras <73215263+jigar-tecmantras@users.noreply.github.com>
2026-07-14 13:56:02 +0530

    feat: add React frontend and Laravel backend

---
+++ backend/config/cors.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => false,
+    'max_age' => false,
+    'supports_credentials' => true,
+];
